ccca3175cda2eba0d8b99f528687a1743845b2e6
Christopher Chew <fishkepr@comcast.net>
2018-01-12 19:53:36 -0800

    Gave up on borders

--- README.md
+++ README.md
@@ -2,8 +2,11 @@
 Lab 02 Project
 
 <ul>
-<Li>JS qiiz with five yes/no questions.</li>
+<Li>JS quiz with five yes/no questions.</li>
 <Li>User must eventually get all questions right to continue.</li>
-<Li>Exit dialog with number wrong depends on number of missed questions.</li>
+<Li>Exit dialog with number wrong varies depending on number of missed questions.</li>
 <Li>'About Me' HTML/CSS page displaying basic information.</li>
 </ul>
+
+<p>After numerous to get borders to work I eventually gave up.</p>
+<p>I was able to get them to work just fine in 101 so I don't understand what I'm doing wrong.
--- js/app.js
+++ js/app.js
@@ -20,73 +20,73 @@ while (userIsRight == false) {
         alert(user + ', yhat is right!  I have one child');
         userIsRight = true;
     } else {
-        console.log('The user answered that I do NOT have a child. Total Wrong: ' + answersWrong);  
         alert(user + ', that is not correct!  Sorry, try again.');
-        answersWrong++
+        answersWrong++;
+        console.log('The user answered that I do NOT have a child. Total Wrong: ' + answersWrong);  
    }
 }
 
 //Question 2:  pets
 userIsRight = false;
 while (userIsRight == false) {
-    var answer = prompt(user + ", Do you think I have any pets?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
+    answer = prompt(user + ", Do you think I have any pets?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
     if (answer == 'YES' || answer == 'Y') {
         console.log('The user answered that I have a pet.'); 
         alert(user + ', that is right!  I have lots of pets');
         userIsRight = true;
     } else {
-        console.log('The user answered that I do NOT have any pets. Total Wrong: ' + answersWrong); 
         alert(user + ', that is not correct!  Sorry, try again.');
-        answersWrong++
+        answersWrong++;
+        console.log('The user answered that I do NOT have any pets. Total Wrong: ' + answersWrong); 
    }
 }
 
 //Question 3:  Puget Sound
 userIsRight = false;
 while (userIsRight == false) {
-    var answer = prompt(user + ", Do you think I live in Puget Sound?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
+    answer = prompt(user + ", Do you think I live in Puget Sound?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
     if (answer == 'YES' || answer == 'Y') {
         console.log('The user answered that I live in Puget Sound.'); 
         alert(user + ', that is right!  \(Yeah, that was not difficult was it?\)');
         userIsRight = true;
     } else {
-        console.log('The user answered that I do NOT live in Puget Sound. Total Wrong: ' + answersWrong); 
         alert(user + ', that is not correct!  Sorry, try again.');
-        answersWrong++
+        answersWrong++;
+        console.log('The user answered that I do NOT live in Puget Sound. Total Wrong: ' + answersWrong); 
    }
 }
 
 //Question 4:  Play Outside
 userIsRight = false;
 while (userIsRight == false) {
-    var answer = prompt(user + ", Do you think I like to \'Play Outside\' as WDFW says?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
+    answer = prompt(user + ", Do you think I like to \'Play Outside\' as WDFW says?    Enter Yes or No \(Y or N is OK too\):").toUpperCase();
     if (answer == 'YES' || answer == 'Y') {
         console.log('The user answered that I like to play outside.'); 
         alert(user + ', that is right!  Hmmm, identifying WDFW probably gave that one away.');
         userIsRight = true;
     } else {
-        console.log('The user answered that I do NOT like to play outside.  Total Wrong: ' + answersWrong); 
         alert(user + ', that is not correct!  Sorry, try again.');
         answersWrong++
+        console.log('The user answered that I do NOT like to play outside.  Total Wrong: ' + answersWrong); 
    }
 }
 
 //Question 5:  Fishing
 userIsRight = false;
 while (userIsRight == false) {
-    var answer = prompt(user + ", Do you think I like to go fishing?    Enter Yes or No (Y or N is OK too):").toUpperCase();
+    answer = prompt(user + ", Do you think I like to go fishing?    Enter Yes or No (Y or N is OK too):").toUpperCase();
     if (answer == 'YES' || answer == 'Y') {
         console.log('The user answered that I like to go fishing.'); 
         alert(user + ', that is right!  Yeah, there\'s that WDFW reference again.');
         userIsRight = true;
     } else {
-        console.log('The user answered that I do NOT like to go fishing.  Total Wrong: ' + answersWrong); 
         alert(user + ', that is not correct!  Sorry, try again.');
-        answersWrong++
+        answersWrong++;
+        console.log('The user answered that I do NOT like to go fishing.  Total Wrong: ' + answersWrong); 
    }
 }
 
-// Exit dialog.  If we are here, the user got everything right.
+// Exit dialog.  If we are here, the user (eventually got everything right.
 console.log('The number of times user answered wrong: ' + answersWrong); 
 
 alert(user + ', you got them all right!  Code Fellows TAs are so smart!');
